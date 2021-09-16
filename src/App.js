36fb5b73397c38d9fdd7a088a38e519654450c19
James Thackeray <jsthackeray@gmail.com>
2021-09-17 00:59:17 +0200

    Styling Authenticator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
-import Projects from './Pages/Projects';
-import Questionnaire from './Pages/Questionnaire';
-import Profile from './Pages/Profile';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Projects from "./Pages/Projects";
+import Questionnaire from "./Pages/Questionnaire";
+import Profile from "./Pages/Profile";
 import Amplify from "aws-amplify";
-import {AmplifyAuthContainer,AmplifyAuthenticator} from "@aws-amplify/ui-react";
+import {
+  AmplifyAuthContainer,
+  AmplifyAuthenticator,
+} from "@aws-amplify/ui-react";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
@@ -13,18 +16,18 @@ Amplify.configure(awsconfig);
 function App() {
   return (
     <AmplifyAuthContainer>
-    <AmplifyAuthenticator>
-    <>
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route path="/" exact component={Projects}/>
-        <Route path="/questionnaire" component={Questionnaire}/>
-        <Route path="/profile" component={Profile}/>
-      </Switch>
-    </Router>
-    </>
-    </AmplifyAuthenticator>
+      <AmplifyAuthenticator>
+        <>
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route path="/" exact component={Projects} />
+              <Route path="/questionnaire" component={Questionnaire} />
+              <Route path="/profile" component={Profile} />
+            </Switch>
+          </Router>
+        </>
+      </AmplifyAuthenticator>
     </AmplifyAuthContainer>
   );
 }
